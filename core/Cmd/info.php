4e43d6fa225bad2dd6a7ad670975732d3ed1a7af
Hind Sagar Biswas <hindsbhk@gmail.com>
2023-10-29 01:47:21 +0600

    info update

--- core/Cmd/info.php
+++ core/Cmd/info.php
@@ -2,7 +2,7 @@
 
 use Hindbiswas\Phpdotenv\StdIO;
 
-$VERSION = 'v0.1.2-beta ';
+$VERSION = 'v0.1.3-beta ';
 $AUTHOR = 'Hind Sagar Biswas ';
 $REPO = 'https://github.com/hind-sagar-biswas/php_launcher ';
 
